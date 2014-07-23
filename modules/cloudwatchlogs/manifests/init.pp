83b84d0f9abc5fc089797f63db058714e77f4e1b
Russ McKendrick <russ.mckendrick@reconnix.com>
2014-07-23 08:36:11 +0100

    Sorting install

--- modules/cloudwatchlogs/manifests/init.pp
+++ modules/cloudwatchlogs/manifests/init.pp
@@ -9,12 +9,32 @@ path => '/var/log/secure',
 },
 ]
 
-file { '/root/test.conf':
-ensure	=> file,
-content	=> template('cloudwatchlogs/awslogs.erb'),
-owner	=> root,
-group	=> root,
-mode	=> 0644,
-}
+
+# Create the configuration file
+
+	file { '/etc/cloudwatchlogs.conf':
+		ensure	=> file,
+		content	=> template('cloudwatchlogs/awslogs.erb'),
+		owner	=> root,
+		group	=> root,
+		mode	=> 0644,
+		notify  => Exec['cloudwatchlogs-wget'],
+	}
+
+
+# Download and install the agent
+
+	exec { 'cloudwatchlogs-wget':
+		command     => 'sudo /usr/bin/wget https://s3.amazonaws.com/aws-cloudwatch/downloads/awslogs-agent-setup-v1.0.py',
+		onlyif      => 'sudo /usr/bin/test -a /etc/cloudwatchlogs.conf'
+		cwd         => '/usr/local/src/',
+		unless      => 'sudo /usr/bin/test -a /usr/local/src/awslogs-agent-setup-v1.0.py',
+		notify      => Exec['cloudwatchlogs-install'],
+	}
+
+	exec { 'cloudwatchlogs-install':
+		command     => 'sudo /usr/bin/python /usr/local/src/awslogs-agent-setup-v1.0.py -n -r us-east-1 -c /etc/cloudwatchlogs.conf',
+		onlyif      => 'sudo /usr/bin/test -a /usr/local/src/awslogs-agent-setup-v1.0.py'
+	}
 
 }
